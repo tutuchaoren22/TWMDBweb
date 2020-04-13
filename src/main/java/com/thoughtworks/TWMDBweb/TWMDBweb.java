3556a17761b8137cb992355a20a160d9d734997c
duyuqian <15738381799@163.com>
2020-04-13 17:11:18 +0800

    [duyuqian] feat: initialize the spring project

---
+++ src/main/java/com/thoughtworks/TWMDBweb/TWMDBweb.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.TWMDBweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TWMDBweb {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TWMDBweb.class, args);
+	}
+
+}
