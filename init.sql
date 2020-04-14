599eb4a4b03d248d00d5915bc474909fb30cdd33
Asuu <suqinzheng_0624@163.com>
2020-04-14 13:33:33 +0800

    [Asu] feat: add init.sql& fastjson dependencies

---
+++ init.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA `movie_sys` DEFAULT CHARACTER SET utf8 ;
+USE movie_sys;
+ALTER table movies ADD PRIMARY KEY (id);
+ALTER TABLE movies ADD summary longtext;
+
+SELECT
+    *
+FROM
+    movies;
