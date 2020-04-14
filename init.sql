78599e5a0efe7af5d0d61404decf7426237bf9d7
duyuqian <15738381799@163.com>
2020-04-14 23:28:08 +0800

    [duyuqian] feat: add the sql statement for add durations in database

--- init.sql
+++ init.sql
@@ -3,7 +3,8 @@ USE movie_sys;
 ALTER TABLE movies change id movie_id int(11);
 ALTER TABLE movies ADD id INT(5) NOT NULL
     PRIMARY KEY AUTO_INCREMENT FIRST;
-ALTER TABLE movies ADD summary longtext;
+ALTER TABLE movies ADD summary LongTEXT;
+ALTER TABLE movies ADD durations TEXT ;
 
 SELECT
     *
