e13d41c0af000562a7cf130fe641e9f8fdbc7cf8
Asuu <suqinzheng_0624@163.com>
2020-04-14 13:38:48 +0800

    [Asu] feat: add config to connect to other's api

--- src/main/java/com/thoughtworks/TWMDBweb/DataSourceConfig.java
+++ src/main/java/com/thoughtworks/TWMDBweb/DataSourceConfig.java
@@ -3,7 +3,10 @@ package com.thoughtworks.TWMDBweb;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.client.RestTemplate;
 
 import javax.sql.DataSource;
 
@@ -18,5 +21,18 @@ public class DataSourceConfig {
         dataSource.setUsername("root");
         dataSource.setPassword("123456");
         return dataSource;
+    }
+
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory){
+        return new RestTemplate(factory);
+    }
+
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory(){
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(15000);
+        factory.setReadTimeout(5000);
+        return factory;
     }
 }
