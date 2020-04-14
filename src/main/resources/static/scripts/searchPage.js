652f7f6097e3fc13138ce2499cdcac3896ccf4ca
Ryan.Wang <ranwang1995@outlook.com>
2020-04-14 17:13:45 +0800

    [WangRan] feat: add the function search by single keyword

--- src/main/resources/static/scripts/searchPage.js
+++ src/main/resources/static/scripts/searchPage.js
@@ -1,44 +1,50 @@
-let movieDb;
 let content = document.getElementsByClassName('content')[0];
 let inputToSearch = decodeURI(window.location.search.slice(1));
 
-function fetchDataFromLocalStorage() {
-    movieDb = readOnServiceDb();
-}
+searchProject(inputToSearch);
 
 function searchProject(data) {
-    let inputValue = document.getElementById("search-movie").value || data;
-    inputValue = (inputValue === undefined) ? ' ' : inputValue;
+  let inputValue = document.getElementById("search-movie").value || data;
+  inputValue = (inputValue === undefined) ? ' ' : inputValue;
+  if (0 !== inputValue.length) {
     content.innerHTML = `<h2>搜索：${inputValue}</h2>`;
-    let searchMovieId = itemSearch(movieDb, inputValue);
-    let searchMovie = movieSearch(searchMovieId);
-    if (inputValue && searchMovie.length !== 0) {
-        searchMovie.forEach(item => {
-            content.innerHTML += `<div class="searchResult">
+    // let searchMovieId = itemSearch(movieDb, inputValue);
+    getMoviesByName(inputValue).then(searchResult => {
+      if (inputValue && searchResult.length !== 0) {
+        searchResult.forEach(item => {
+          content.innerHTML += `<div class="searchResult">
             <div class="picAndDetail">
-            <a href="../pages/detailPage.html?${item.id}" target="_blank"><img src="${item.images.small}" /></a>
+            <a href="../pages/detailPage.html?${item.movieId}" target="_blank"><img src="${item.image}" /></a>
             <div class="movie-details">
             <p>${item.title}</p>
-            <p>导演：${item.directors.map(item => item.name).join(',')}</p>
-            <p>主演：${item.casts.map(item => item.name).join(',')}</p>
-            <p>类型：${item.genres.join(',')}</p>
-            <p>评分：${item.rating.average}</p>
+            <p>导演：${item.directors}</p>
+            <p>主演：${item.casts}</p>
+            <p>类型：${item.genres}</p>
+            <p>评分：${item.rating}</p>
       </div></div></div>`;
         });
-    } else {
+      } else {
         content.innerHTML += `<div class="searchResult">没有找到关于"${inputValue}"的电影，换个搜索词试试吧~</div>`;
-    }
+      }
+    });
+  }
 }
 
 function keyEnterSearchProject(event) {
-    if (13 == event.keyCode) {
-        searchProject();
-    }
+  if (13 == event.keyCode) {
+    searchProject();
+  }
 }
 
-function movieSearch(searchMovieId) {
-    return movieDb.filter(movie => (searchMovieId.indexOf(movie.id) !== -1));
+function getMoviesByName(name) {
+  return new Promise(function (resolve, reject) {
+    let AJAXSetup = {
+      url: 'http://localhost:8080/api/search?text=' + name,
+      method: 'GET',
+      success: function (result) {
+        resolve(result);
+      }
+    };
+    AJAXHandle(AJAXSetup);
+  });
 }
-
-initDb(fetchDataFromLocalStorage);
-searchProject(inputToSearch);
