eb4b7eb316fde18df6f26d077c210cac631cc6c2
Ryan.Wang <ranwang1995@outlook.com>
2020-04-15 14:03:11 +0800

    [WangRan] feat: add movie detail page

--- src/main/resources/static/scripts/detailPage.js
+++ src/main/resources/static/scripts/detailPage.js
@@ -1,145 +1,145 @@
-const BASIC_URL = 'http://127.0.0.1:8888';
 let movieId = window.location.search.slice(1);
-let movieDb;
-let classificationDb;
+let movieGenres;
 let movieInformations = document.getElementsByClassName('movie-informations')[0];
 let movieSummarys = document.getElementsByClassName('movie-summary')[0];
 let movieComments = document.getElementsByClassName('movie-comments')[0];
 let movieRecommend = document.getElementsByClassName('recommend-information')[0];
 
-function fetchDataFromLocalStorage() {
-    classificationDb = readDbClassification();
-    movieDb = readOnServiceDb();
+run();
+
+function run() {
+  getMovieData().then(result => {
+    loadMovieDetail(result);
+    getMovieRecommends();
+  });
+  getMovieReviews().then(result => {
+    loadMovieReviews(result);
+  });
 }
 
 function getMovieData() {
+  return new Promise(function (resolve, reject) {
     let options = {
-        url: BASIC_URL + '/v2/movie/subject/' + movieId,
-        method: 'GET',
-        success: function(res) {
-            loadMovieDetail(res);
-        },
-        fail: function(error) {
-            console.log('ERROR');
-        }
+      url: 'http://localhost:8080/api/movie?movieId=' + movieId,
+      method: 'GET',
+      success: function (res) {
+        resolve(res);
+      },
+      fail: function (error) {
+        console.log('ERROR');
+      }
     };
     AJAXHandle(options);
+  });
 }
 
 function loadMovieDetail(data) {
-    let movieDirectors = data.directors.map(item => item.name).join(',');
-    let movieCasts = data.casts.map(item => item.name).join(',');
-    let movieGenres = data.genres.join(',');
-    let movieCountries = data.countries.join(',');
+  let movieCountries = '待补充！！！';
 
-    movieInformations.innerHTML = `<h2>${data.title}——${data.original_title}</h2>
+  movieInformations.innerHTML = `<h2>${data.title}——${data.originalTitle}</h2>
     <div class="picAndDetail">
-    <img src="${data.images.small}" />
+    <img src="${data.image}" alt="电影海报图" />
     <div class="movie-details">
-    <p>导演：${movieDirectors}</p>
-    <p>主演：${movieCasts}</p>
-    <p>类型：${movieGenres}</p>
+    <p>导演：${data.directors}</p>
+    <p>主演：${data.casts}</p>
+    <p>类型：${data.genres}</p>
     <p>制片国家/地区：${movieCountries}</p>
     <p>上映日期：${data.year}</p>
     <p>片长：${data.durations}</p>
-    <p>评分：${data.rating.average}</p></div></div>`;
-    movieSummarys.innerHTML = `<h3>剧情简介</h3><p>${data.summary}</p>`;
+    <p>评分：${data.rating}</p></div></div>`;
+  movieSummarys.innerHTML = `<h3>剧情简介</h3><p>${data.summary}</p>`;
+  movieGenres = data.genres.split(',')[0];
 }
 
 function getMovieReviews() {
+  return new Promise(function (resolve, reject) {
     let options = {
-        url: BASIC_URL + '/v2/movie/subject/' + movieId + '/comments?start=1&count=100',
-        method: 'GET',
-        success: function(res) {
-            loadMovieReviews(res);
-        },
-        fail: function(error) {
-            console.log('ERROR');
-        }
+      url: 'http://localhost:8080/api/comments?movieId=' + movieId,
+      method: 'GET',
+      success: function (res) {
+        resolve(res);
+      },
+      fail: function (error) {
+        console.log('ERROR');
+      }
     };
     AJAXHandle(options);
+  });
 }
 
 function loadMovieReviews(data) {
-    let comments = sortCommentsWithTime(data.comments).slice(0, 5);
-    movieComments.innerHTML += `<h3>电影热评</h3>
+  let comments = sortCommentsWithTime(data).slice(0, 5);
+  movieComments.innerHTML += `<h3>电影热评</h3>
     <div class="user-comments"></div>`;
-    let userComments = document.getElementsByClassName('user-comments')[0];
-    comments.forEach(element => {
-        userComments.innerHTML += `
+  let userComments = document.getElementsByClassName('user-comments')[0];
+  comments.forEach(element => {
+    userComments.innerHTML += `
       <div class='user-comment'>
-      <span>${element.author.name}</span>
-      <span>${ratingToStar(element.rating.value)}</span>
-      <span> ${element.created_at.split(' ')[0]}</span>
-      <p>${element.content}</p>
+      <span>${element.authorName}</span>
+      <span>${ratingToStar(element.rating)}</span>
+      <span> ${element.commentDate}</span>
+      <p>${element.comments}</p>
       </div>`;
-    });
+  });
 }
 
 function sortCommentsWithTime(comments) {
-    return comments.sort(function (movieA, movieB) {
-        let yearA = Number(movieA.created_at.split(/[-: ]/).join(''));
-        let yearB = Number(movieB.created_at.split(/[-: ]/).join(''));
-        return yearB - yearA;
-    });
+  return comments.sort(function (movieA, movieB) {
+    let yearA = Number(movieA.commentDate.split(/[-: ]/).join(''));
+    let yearB = Number(movieB.commentDate.split(/[-: ]/).join(''));
+    return yearB - yearA;
+  });
 }
 
 function ratingToStar(num) {
-    return "★".repeat(num) + "☆".repeat(5 - num);
+  return "★".repeat(num) + "☆".repeat(5 - num);
 }
 
-function loadMovieRecommend(movieId) {
-    let data = movieDb.filter(
-        item => (item.id === movieId)
-    )[0];
-    let movieTitle = data.title;
-    let movieRating = data.rating.average;
-    let movieImages = data.images.small;
-    let movieURL = `../pages/detailPage.html?${movieId}`;
-    movieRecommend.innerHTML += `
+function loadMovieRecommend(data) {
+  data.forEach(movieObj => {
+    if (movieObj.movieId !== movieId) {
+      let movieURL = `../pages/detailPage.html?${movieObj.movieId}`;
+      movieRecommend.innerHTML += `
     <div class="one-movie-recommend">
       <a href=${movieURL}>
-        <img src="${movieImages}" />
-        <p>${movieTitle}</p>
+        <img src="${movieObj.image}" />
+        <p>${movieObj.title}</p>
       </a>
-        <p>评分：${movieRating}</p>
+        <p>评分：${movieObj.rating}</p>
     </div>`;
+    }
+  });
+
 }
 
-function getMovieRecommendId(movieId) {
-    let data = movieDb.filter(
-        item => (item.id === movieId)
-    );
-    let recommendData = classificationDb.filter(
-        item => (item.name === data[0].genres[0])
-    );
-    let recommendMovieIdList = recommendData[0].id.filter(
-        item => (item !== movieId)
-    );
-    return recommendMovieIdList.slice(0, 6);
-}
 
 function getMovieRecommends() {
-    let recommendMovieIdList = getMovieRecommendId(movieId);
-    recommendMovieIdList.forEach(
-        item => loadMovieRecommend(item)
-    );
+  getMoviesOfCategory(movieGenres, 1, 6).then(result => {
+    loadMovieRecommend(result);
+  });
 }
 
 function keyEnterSearchProject() {
-    if (13 == event.keyCode) {
-        searchProject();
-    }
+  if (13 == event.keyCode) {
+    searchProject();
+  }
 }
 
 function searchProject() {
-    let inputValue = document.getElementById("search-movie").value;
-    window.open(`../pages/searchPage.html?${inputValue}`, '_blank');
+  let inputValue = document.getElementById("search-movie").value;
+  window.open(`../pages/searchPage.html?${inputValue}`, '_blank');
 }
 
-initDb(() => {
-    fetchDataFromLocalStorage();
-    getMovieData();
-    getMovieReviews();
-    getMovieRecommends();
-});
+function getMoviesOfCategory(category, start, end) {
+  return new Promise(function (resolve, reject) {
+
+    let AJAXSetup = {
+      url: 'http://localhost:8080/api/category?category=' + category + '&start=' + start + '&end=' + end,
+      method: 'GET',
+      success: function (result) {
+        resolve(result);
+      }
+    };
+    AJAXHandle(AJAXSetup);
+  });
+}
